335be8d5eb94ab742b3f3b8518e19a2a4cfc18d7
tami2408 <tamara.kaselj@student.htl-rankweil.at>
2026-06-11 12:13:03 +0200

    logging eingebaut und fertig

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import model
 import uvicorn
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from starlette import status
+
 from database import Base, engine
+from logger_config import logger
+
 from users.router import router as user_router
 from ordner.router import router as ordner_router
 from karteikarten.router import router as karteikarten_router
@@ -10,8 +16,38 @@ from statistik.router import router as statistik_router
 
 
 
+
 Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Karteikartensystem", description="Karteikartensystem", version="1.0.0")
+
+@app.middleware("http")
+async def log_request(request: Request, call_next):
+    start_time = time.time()
+
+    logger.info(f"Request gestartet: {request.method} {request.url.path}")
+
+    try:
+        response = await call_next(request)
+
+        dauer = round(time.time() - start_time, 4)
+
+        logger.info(
+            f"Request beendet: {request.method} {request.url.path} "
+            f"Statuscode: {response.status_code} Dauer: {dauer}s"
+        )
+
+        return response
+
+    except Exception as fehler:
+        dauer = round(time.time() - start_time, 4)
+
+        logger.error(
+            f"Fehler bei Request: {request.method} {request.url.path} "
+            f"Dauer: {dauer}s Fehler: {str(fehler)}")
+
+        return JSONResponse(status_code=500, content={"detail": "Interner Serverfehler. Bitte überprüfen die Logs."})
+
+
 
 app.include_router(user_router)
 app.include_router(ordner_router)
@@ -22,6 +58,7 @@ app.include_router(statistik_router)
 
 @app.get("/") 
 def root():
+    logger.info("Root-Endpunkte wurden aufgerufen.")
     return {"message": "Unser Karteikartensystem."}
 
 if __name__ == "__main__":
